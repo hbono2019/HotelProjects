3fe66018914aa47d7e987a83cbc1dd62d4f536ef
hbono2019 <d18129993@mytudublin.ie>
2019-11-19 20:34:59 +0000

    Sixty Fourth Release added extra features for the FK
    Signed-off-by: hbono2019 <D18129993@mytudublin.ie>

--- HotelsAppCRUD/models.py
+++ HotelsAppCRUD/models.py
@@ -51,7 +51,7 @@ class Room(models.Model):
         (CONCIERGE_RSRV, 'Concierge Reservation'),
     )
     room_id = models.AutoField(primary_key=True)
-    hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE, related_name='room_hotel')
     room_number = models.IntegerField()
     room_type = models.CharField(max_length=60, choices=ROOM_TYPE, default=ECONOMY)
     type = models.CharField(max_length=120, choices=TYPE_RESRV_ROOM, default=ONE_TIME_RSRV)
@@ -64,9 +64,9 @@ class Room(models.Model):
 
 class RoomReservation(models.Model):
     reservation_id = models.AutoField(primary_key=True)
-    hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE)
-    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, default='')
-    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    hotel_id = models.ForeignKey(Hotel, on_delete=models.CASCADE, related_name='reservation_hotel')
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='reservation_customer', default='')
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='reservation_room')
     from_date = models.DateField()
     to_date = models.DateField()
     number_of_adults = models.IntegerField()
@@ -86,8 +86,8 @@ class RoomService(models.Model):
 
 class RoomCharges(models.Model):
     room_charge_id = models.AutoField(primary_key=True)
-    reservation_id = models.ForeignKey(RoomReservation, on_delete=models.CASCADE)
-    service_id = models.ForeignKey(RoomService, on_delete=models.CASCADE)
+    reservation_id = models.ForeignKey(RoomReservation, on_delete=models.CASCADE, related_name='charge_reservation')
+    service_id = models.ForeignKey(RoomService, on_delete=models.CASCADE,related_name='charge_service')
     description = models.CharField(max_length=120, default='')
     discount_code = models.CharField(max_length=20, default='', blank=True, null=True)
     discount_price = models.DecimalField(max_digits=5, decimal_places=2, default='')
@@ -97,8 +97,8 @@ class RoomCharges(models.Model):
 
 class RoomBilling(models.Model):
     reservation_id = models.AutoField(primary_key=True)
-    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    room_charge_id = models.ForeignKey(RoomCharges, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='billing_customer')
+    room_charge_id = models.ForeignKey(RoomCharges, on_delete=models.CASCADE, related_name='billing_charge')
     invoice_no = models.CharField(max_length=120, default='')
     billing_address1 = models.CharField(max_length=120)
     billing_address2 = models.CharField(max_length=120, blank=True, null=True)
